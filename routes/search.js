903059d521494c1ae5338de6092299870e64c01c
henrfos <henrik.fossing17@gmail.com>
2024-05-26 22:56:47 +0200

    implements API

--- routes/search.js
+++ routes/search.js
@@ -2,20 +2,31 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/', (req, res) => {
+const API_KEY = '6a0af1fd';
+
+router.get('/', async (req, res) => {
   const query = req.query.q;
   if (query) {
-    axios.get(`dummyapikey`)
-      .then(response => {
-        res.render('search', { movies: response.data.Search });
-      })
-      .catch(error => {
-        console.error(error);
-        res.render('search', { movies: [] });
-      });
+    try {
+      const searchResponse = await axios.get(`http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`);
+      let movies = searchResponse.data.Search || [];
+      
+      // Fetch detailed information for each movie
+      movies = await Promise.all(movies.map(async (movie) => {
+        const detailsResponse = await axios.get(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=${API_KEY}`);
+        return detailsResponse.data;
+      }));
+      
+      res.render('search', { movies });
+    } catch (error) {
+      console.error(error);
+      res.render('search', { movies: [] });
+    }
   } else {
     res.render('search', { movies: [] });
   }
 });
 
 module.exports = router;
+
+//http://www.omdbapi.com/?apikey=6a0af1fd&

2fa5621116993142cbdbb6c0bfc35e8bd90143ec
Henrik Fossing <henrik.fossing17@gmail.com>
2025-03-31 21:20:22 +0200

    readability

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const LocalStrategy = require('passport-local').Strategy;
+const { Strategy: LocalStrategy } = require('passport-local');
 const bcrypt = require('bcrypt');
 const db = require('../models');
 
@@ -7,30 +7,26 @@ module.exports = (passport) => {
     async (username, password, done) => {
       try {
         const user = await db.User.findOne({ where: { username } });
-        if (!user) {
-          return done(null, false, { message: 'Incorrect username.' });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-          return done(null, false, { message: 'Incorrect password.' });
-        }
+        if (!user) return done(null, false, { message: 'Incorrect username.' });
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) return done(null, false, { message: 'Incorrect password.' });
+
         return done(null, user);
-      } catch (error) {
-        return done(error);
+      } catch (err) {
+        return done(err);
       }
     }
   ));
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+  passport.serializeUser((user, done) => done(null, user.id));
 
   passport.deserializeUser(async (id, done) => {
     try {
       const user = await db.User.findByPk(id);
       done(null, user);
-    } catch (error) {
-      done(error, null);
+    } catch (err) {
+      done(err);
     }
   });
 };
